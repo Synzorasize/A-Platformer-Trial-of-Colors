f87c3001fc7d0b65166ce678d0d70e917a70ed83
Synzorasize <60207816+Synzorasize@users.noreply.github.com>
2021-04-04 05:42:48 -0500

    Removed comments

--- Levels/CustomLevel.gd
+++ Levels/CustomLevel.gd
@@ -10,28 +10,15 @@ export (Dictionary) var data = {
 	},
 	"Tile": Vector2(384,224),
 }
-#var level : PackedScene = PackedScene.new()
-#var level_names = []
 onready var Player = $Player
 onready var Camera = $Player/Camera2D
 onready var UI = $CanvasLayer/UI
 var RenameLevelText
 var descendants = []
-#var export : bool = false
-#onready var FD = $"../FileDialog"
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-#func iterate_names():
-#	level_names.clear()
-#	if not GlobalVariables.userdata.saved_levels.empty():
-#		for i in GlobalVariables.userdata.saved_levels:
-#			level_names.append(GlobalVariables.userdata.saved_levels[i["name"]])
 
 func restart():
 	get_tree().change_scene_to(GlobalVariables.level.duplicate())
 
-# Called when the node enters the scene tree for the first time.
 func _enter_tree():
 	GlobalVariables.LevelNum = 999
 func _ready():
@@ -46,9 +33,6 @@ func _ready():
 	else:
 		RenameLevelText = $"../Level Editor/Panel4/LineEdit2"
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
 func saveLevel():
 	position = Vector2(0,0)
 	descendants.clear()
@@ -70,24 +54,13 @@ func _on_SaveButton_pressed():
 func _on_PlayButton_pressed():
 	UI.show()
 	saveLevel()
-#	ResourceSaver.save("res://current_scene.tscn", GlobalVariables.level)
-#	print(Player.get_signal_connection_list("iamred"))
 	get_tree().change_scene_to(GlobalVariables.level)
 	GlobalVariables.editor = false
 	GlobalVariables.editor_playing = true
 
 
 func _on_ExportButton_pressed():
-#	export = true
 	saveLevel()
-#	FD.popup()
-
-#func _on_FileDialog_confirmed():
-#	if export == true:
-#		ResourceSaver.save(FD.get_current_path(), GlobalVariables.level)
-#		export = false
-	
-
 
 func _on_CameraButton_toggled(button_pressed):
 	if button_pressed:
@@ -97,6 +70,5 @@ func _on_CameraButton_toggled(button_pressed):
 
 
 func _on_RenameButton2_pressed():
-	#print(GlobalVariables.userdata.saved_levels[0].level_name)
 	data.level_name = RenameLevelText.text
 	get_parent().RenamePanel.hide()
